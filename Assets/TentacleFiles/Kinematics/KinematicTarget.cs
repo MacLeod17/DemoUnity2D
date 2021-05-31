cf494414e51c786e2a17a4b0c931b400b1873eee
Chandler Price <61471918+ChandlerPrice@users.noreply.github.com>
2021-05-30 18:45:18 -0600

    Kinematic Files

---
+++ Assets/TentacleFiles/Kinematics/KinematicTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicTarget : MonoBehaviour
+{
+	[SerializeField] Camera m_camera = null;
+
+	private void Start()
+	{
+		m_camera = (m_camera == null) ? Camera.main : m_camera;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Vector2 mousePosition = Input.mousePosition;
+			Vector2 position = m_camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 0));
+			position = new Vector3(position.x, position.y, -2);
+
+			transform.position = position;
+		}
+	}
+}
