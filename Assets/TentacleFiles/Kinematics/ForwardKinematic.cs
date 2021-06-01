59e18b0aab1b24d688b4835906d34ec676545c19
Chandler Price <61471918+ChandlerPrice@users.noreply.github.com>
2021-06-01 15:44:50 -0600

    Tentaculus arm taper

--- Assets/TentacleFiles/Kinematics/ForwardKinematic.cs
+++ Assets/TentacleFiles/Kinematics/ForwardKinematic.cs
@@ -8,6 +8,7 @@ public class ForwardKinematic : MonoBehaviour
 	[Range(1, 40)] public int count = 5;
 	[Range(0.0f, 10.0f)] public float length = 1;
 	[Range(0.0f, 10.0f)] public float width = 1;
+	[Range(0.0f, 20.0f)] public float taper = 10;
 	[Range(0.0f, 10.0f)] public float noiseRate = 1;
 
 	List<ForwardKinematicSegment> segments = new List<ForwardKinematicSegment>();
@@ -22,7 +23,7 @@ public class ForwardKinematic : MonoBehaviour
 		for (int i = 0; i < count; i++)
 		{
 			ForwardKinematicSegment segment = Instantiate(segmentOriginal, transform);
-			segment.Initialize(parent, position, angle, length, width);
+			segment.Initialize(parent, position, angle, (length - (i/taper)), (width - (i/taper)));
 			segments.Add(segment);
 
 			parent = segment;
@@ -34,8 +35,8 @@ public class ForwardKinematic : MonoBehaviour
 		int count = 1;
 		foreach (ForwardKinematicSegment segment in segments)
 		{
-			segment.length = length;
-			segment.width = width;
+			segment.length = length - ((count-1)/taper);
+			segment.width = width - ((count - 1) / taper);
 			segment.noiseRate = noiseRate * ((float)count / (float)segments.Count);
 			count++;
 
